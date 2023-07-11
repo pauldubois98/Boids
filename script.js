1822fd0e5f627c45939ba69cdf7193fefbbc1eaf
pauldubois98 <pauldubois98@gmail.com>
2023-07-11 22:32:24 +0200

    draw boids (basic)

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ function init() {
     boids.push(new Boid(simulation.width / 2, simulation.height / 2, vx, vy));
   }
 }
+
+function draw() {
+  ctx.clearRect(0, 0, simulation.width, simulation.height);
+  for (let i = 0; i < boids.length; i++) {
+    const boid = boids[i];
+    ctx.beginPath();
+    ctx.moveTo(boid.x, boid.y);
+    ctx.lineTo(boid.x + boid.vx * 10, boid.y + boid.vy * 10);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(boid.x, boid.y, 5, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+}
+
+function update() {
+  for (let i = 0; i < boids.length; i++) {
+    const boid = boids[i];
+    boid.x += boid.vx;
+    boid.y += boid.vy;
+  }
+}
+
+init();
+setInterval(() => {
+  update();
+  draw();
+}, 1000 / 60);
